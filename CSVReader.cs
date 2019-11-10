82e6b3503b9ab6ac9da52b2b791d94b98d60bb5b
Mayan Shoshani <mshoshan@uci.edu>
2019-11-09 16:09:37 -0800

    initial upload
    added the CSVReader and basic dialogue object

---
+++ CSVReader.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+/// <summary>
+/// reads  csvs made from google spreadsheets,
+/// returns list of lines, if number of lines is less that one returns an empty list
+/// first line of csv will denote the names that all other info is stored under in the the dictionary, in that specific order, ie everything under the name col can accesed with obj[name]
+///Example
+/// name, text, image
+/// Bob, hello world,image1 
+/// </summary>
+public class CSVReader
+{
+    static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+    static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+    static char[] TRIM_CHARS = { '\"' };
+
+    public static List<Dictionary<string, object>> Read(string file)
+    {
+        var list = new List<Dictionary<string, object>>();
+        TextAsset data = Resources.Load(file) as TextAsset;
+
+        var lines = Regex.Split(data.text, LINE_SPLIT_RE);
+
+        if (lines.Length <= 1) return list;
+
+        var header = Regex.Split(lines[0], SPLIT_RE);
+        for (var i = 1; i < lines.Length; i++)
+        {
+
+            var values = Regex.Split(lines[i], SPLIT_RE);
+            if (values.Length == 0 || values[0] == "") continue;
+
+            var entry = new Dictionary<string, object>();
+            for (var j = 0; j < header.Length && j < values.Length; j++)
+            {
+                string value = values[j];
+                value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
+                object finalvalue = value;
+                int n;
+                float f;
+                if (int.TryParse(value, out n))
+                {
+                    finalvalue = n;
+                }
+                else if (float.TryParse(value, out f))
+                {
+                    finalvalue = f;
+                }
+                entry[header[j]] = finalvalue;
+            }
+            list.Add(entry);
+        }
+        return list;
+    }
+}
---
+++ Dialogue.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//struct for portrait data, keeps track of what type of portrait to show, and for how long
+public struct Portrait
+{
+    public string type;
+    public float time;
+    public Portrait(string typ, float tim)
+    {
+        type = typ;
+        time = tim;
+    }
+}
+
+/// <summary>
+/// dialouge class, stores short text , longText, speaker, line ID, default options in case of timers, options or a list of other lines to look at next given choice, portrait transitions in case want more than one  
+/// basic setup, modify as necessary for your game and what information you want to be stored for every dialouge scene, could add things like time for timer etc
+    /// </summary>
+
+public class Dialogue
+{
+    public string
+        shortText,//text that displays when this dialogue option is listed as a choice
+        longText,//the resulting full text that results from clicking the shortText as a button
+        speaker;//who is speaking "Gavin" for gav, anything else for the perp
+    public int ID,
+        defaultOption;//the unique identifier for this diaglogue object
+    public float maxTimer, maxStressTimePenalty;//max time and max time lost due to stress, respectively
+    public List<int> options;//IDs of other Dialogue entries that are options in response to this dialogue.
+    public List<Portrait> portraitTransitions;//list of portrait types as well as times to set the character talking to, visually
+
+    public Dialogue(int id, Dictionary<string, object> input)
+    {
+        shortText = (string)input["shortText"];
+        longText = (string)input["longText"];
+        speaker = (string)input["speaker"];
+        ID = id + 2;//the spreadsheet is offset by 2 from the true value so we store it as its marked
+        defaultOption = (int)input["default"];
+        //building options list
+        int n = 0;
+        options = new List<int>();
+        string s = (string)input["options"];
+        foreach(string st in s.Replace("a", "").Split(','))
+        {
+            ++n;
+            options.Add(int.Parse(st.Trim()));
+        }
+        
+        //building portrait list using n from last list
+        // formatting for portraittransitions:  (nameOfPortrait,timeToChange)
+        //if do not want portrait to change timeToChange = 0
+        s = (string)input["portraitTransitions"];
+        portraitTransitions = new List<Portrait>();
+        toGrabFrom = s.Replace("(", "").Replace(")", "").Replace(" ", "").Split(',');
+        for (int i = 0; i < toGrabFrom.Length; i += 2)
+        {
+            portraitTransitions.Add(new Portrait(toGrabFrom[i], float.Parse(toGrabFrom[i + 1])));
+        }
+    }
+}
